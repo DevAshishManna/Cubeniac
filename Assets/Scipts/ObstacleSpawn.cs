ff6e2d354add44291cfd3b7ebcc047d4b10f4cf2
LAPTOP-E4LRG7BI\Devashish <mannad199815@gmail.com>
2018-07-27 23:40:20 +0530

    Obstecles are going through the ground

---
+++ Assets/Scipts/ObstacleSpawn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawn : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject obstacle;
+
+    public float amountOfObstacle;
+    public float minX, maxX;
+
+    public float spawnTime = 2f;            // How long between each spawn.
+    public float fallSpeed = 40.0f;    //The speed of falling Apples
+    private float timer = 0;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;   // Timer Counter
+        if (timer > spawnTime)
+        {
+            SpawnRandom();       //Calling method SpawnRandom()
+            timer = 0;        //Reseting timer to 0
+        }
+
+    }
+
+    public void SpawnRandom()
+    {
+        for (int i = 0; i < amountOfObstacle; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(minX, maxX), Random.Range(player.transform.localPosition.y * 2, transform.localPosition.y * 3), 0);
+            Instantiate(obstacle.transform, pos, Quaternion.identity);
+        }
+    }
+}
