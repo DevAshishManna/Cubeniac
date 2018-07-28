3a5960c052ea057d101e690c759ef3373b1f0f01
LAPTOP-E4LRG7BI\Devashish <mannad199815@gmail.com>
2018-07-28 17:45:10 +0530

    player moving currectly obstacles falling correctly

--- Assets/Scipts/GravityEffect.cs
+++ Assets/Scipts/GravityEffect.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class GravityEffect : MonoBehaviour {
 
-    public Collider coll;
+   
 
     void Start()
     {
-        coll = GetComponent<Collider>();
-        coll.isTrigger = true;
+        
     }
 
     // Disables gravity on all rigidbodies entering this collider.
-    void OnTriggerEnter(Collider other)
+    void OnCollisionEnter(Collision other)
     {
-        if (other.attachedRigidbody)
-            other.attachedRigidbody.useGravity = false;
+        print("HI");
+
+        if (other.gameObject.tag != "Friendly")
+        {
+            print("HI");
+            other.collider.attachedRigidbody.useGravity = false;
+            //other.attachedRigidbody.isKinematic = true;
+        }
+                    
     }
 }
--- Assets/Scipts/Player.cs
+++ Assets/Scipts/Player.cs
@@ -6,7 +6,6 @@ public class Player : MonoBehaviour {
 
     private float score;
     public float movementSpeed;
-    Vector3 m_EulerAngleVelocity;
 
     private bool movingLeft;
     public Rigidbody rb;
@@ -14,12 +13,10 @@ public class Player : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        m_EulerAngleVelocity = new Vector3(0, 0, 100);
         rb = GetComponent<Rigidbody>();
         score = 0;
         movingLeft = true;
 
-		
 	}
 	
 	// Update is called once per frame
@@ -34,15 +31,12 @@ public class Player : MonoBehaviour {
         if (movingLeft)
         {
             rb.AddForce(-movementSpeed,0,0,ForceMode.Acceleration);
-            Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
-
+            
         }
         else
         {
             rb.AddForce(movementSpeed,0,0,ForceMode.Acceleration);
-            Quaternion deltaRotation = Quaternion.Euler(-m_EulerAngleVelocity * Time.deltaTime);
-            rb.MoveRotation(rb.rotation * deltaRotation);
+  
         }
 	}
 }
