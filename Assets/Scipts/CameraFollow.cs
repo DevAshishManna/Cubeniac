814e1bcf795e6a2776bcd71972c46c2b5b8b3d1a
LAPTOP-E4LRG7BI\Devashish <mannad199815@gmail.com>
2018-08-01 19:57:42 +0530

    camera improvements (initial)

--- Assets/Scipts/CameraFollow.cs
+++ Assets/Scipts/CameraFollow.cs
@@ -6,13 +6,37 @@ public class CameraFollow : MonoBehaviour {
     public float smoothSpeed = 10f;
     public Vector3 offset;
     private Vector3 velocity = Vector3.zero;
+    float lockPos = 0f;
+    private Vector3 newPos;
 
-    private void FixedUpdate()
+    /*
+    private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position,desiredPosition,ref velocity,smoothSpeed * Time.deltaTime);
-        transform.position = smoothPosition;
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+    }
+    */
 
-        transform.LookAt(target);
+    private void Start()
+    {
+
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+        newPos.y = offset.y;
+        newPos.z = offset.z;
+    }
+
+    private void LateUpdate()
+    {
+        
+        newPos.x = target.position.x + offset.x;
+        
+
+        //transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothSpeed * Time.deltaTime);
+
+        //Vector3 smoothPosition = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+
+        transform.position = smoothPosition;
+        
+
     }
 }
